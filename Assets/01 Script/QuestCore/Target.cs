1471a39708019fe746af4d9d88a3d1342a4d3b11
Nutubit17 <koobonhoo@gmail.com>
2024-01-02 13:57:32 +0900

    240102 1:57 StartScene (1/2)

--- Assets/01 Script/QuestCore/Target.cs
+++ Assets/01 Script/QuestCore/Target.cs
@@ -1,8 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Target 
+public abstract class Target<T> : MonoBehaviour where T : Quest
 {
+    public T Instance;
+
+    public virtual void Start()
+    {
+        QuestPack.Instance.AddToQuest(typeof(T).ToString(), this);        
+    }
+
+    public void ResetTarget()
+    {
+        QuestPack.Instance.AddToQuest(typeof(T).ToString(), this);
+    }
     
+    public abstract void Init();
+    public abstract void Stop();
 }
